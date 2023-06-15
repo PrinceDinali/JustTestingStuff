745dcf1041652eb6513e8451858d68ae46f5628e
PrinceDinali <dinali.assylbek.2003@gmail.com>
2023-06-15 13:50:54 -0600

    I just changed the main.dart app bar title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'This is not a flutter demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
